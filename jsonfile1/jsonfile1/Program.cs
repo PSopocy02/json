461b13ba7ec98f8a6511229f5b033efc2673a3a6
peytonsopocy <peytonsopocy@PEYTONSOPOC61D6>
2026-04-30 10:26:09 -0500

    push

---
+++ jsonfile1/jsonfile1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text.Json;
+
+public class Order
+{
+    public string Customer { get; set; }
+    public string Pizza { get; set; }
+
+    public int Quantity { get; set; }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        string json = File.ReadAllText("PizzaOrder.json");
+
+        List<Order> orders = JsonSerializer.Deserialize<List<Order>>(json);
+
+        foreach (Order order in orders)
+        {
+            Console.WriteLine($"{order.Customer} ordered {order.Quantity} {order.Pizza}(s).");
+        }
+    }
+        
+}
